ccf01e11101f58567919ff7979d5b494215fbc1b
citronoiseau <avroraka2004@gmail.com>
2023-07-27 19:50:55 +0300

    Added links to github and netlify

--- src/Footer.js
+++ src/Footer.js
@@ -2,8 +2,8 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      The project was coded by Anastasiia and is open-sourced on <a href="/" target="_blank">GitHub </a> and
-      hosted on  <a href="/" target="_blank"> Netlify </a> 
+      The project was coded by Anastasiia and is open-sourced on <a href="https://github.com/citronoiseau/react-weather-app" target="_blank">GitHub </a> and
+      hosted on  <a href="https://super-peony-f7481f.netlify.app/" target="_blank"> Netlify </a> 
     </div>
   );
 }
