eeb6e2a6c76a8c923b975563e21b61708b5887ba
citronoiseau <avroraka2004@gmail.com>
2023-07-27 20:03:43 +0300

    Added rel="noreferrer" and link to personal website

--- src/Footer.js
+++ src/Footer.js
@@ -2,8 +2,8 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      The project was coded by Anastasiia and is open-sourced on <a href="https://github.com/citronoiseau/react-weather-app" target="_blank">GitHub </a> and
-      hosted on  <a href="https://super-peony-f7481f.netlify.app/" target="_blank"> Netlify </a> 
+      The project was coded by <a href="https://jolly-trifle-0ca0f0.netlify.app/" target="_blank" rel="noreferrer">Anastasiia </a> and is open-sourced on <a href="https://github.com/citronoiseau/react-weather-app" target="_blank" rel="noreferrer">GitHub </a> and
+      hosted on  <a href="https://super-peony-f7481f.netlify.app/" target="_blank" rel="noreferrer"> Netlify </a> 
     </div>
   );
 }
