0f4c2aacca60f0dfadc56dfadd36bcbac9ecf6db
citronoiseau <avroraka2004@gmail.com>
2023-10-02 15:02:45 +0300

    Fixed the bug with months

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,35 +1,44 @@
 import React from "react";
 
-
 export default function FormattedDate(props) {
-    let days =  ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-      ];
-    let day  = days[props.date.getDay()];
-    let hours = props.date.getHours();
-    if (hours <10) {
-        hours= `0${hours}`
-    }
-    let minutes = props.date.getMinutes();
-    if (minutes  < 10) {
-        minutes = `0${minutes}`
-    }
-    let month = months[props.date.getMonth()];
-    let date = props.date.getDate();
-    return (
-        <div className="date">  Last updated at: {date}th of {month}, {day}, {hours}:{minutes} </div>
-    );
- 
-
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let day = days[props.date.getDay()];
+  let hours = props.date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = props.date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let month = months[props.date.getMonth()];
+  let date = props.date.getDate();
+  return (
+    <div className="date">
+      {" "}
+      Last updated at: {date}th of {month}, {day}, {hours}:{minutes}{" "}
+    </div>
+  );
 }
