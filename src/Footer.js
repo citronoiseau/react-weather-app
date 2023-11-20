c6072237a7df9b9f8a95a29f654e956cce9dd5f3
citronoiseau <avroraka2004@gmail.com>
2023-11-20 10:54:42 +0200

    updated footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,31 @@
-
 export default function Footer() {
   return (
     <div className="Footer">
-      This project was coded by <a href="https://jolly-trifle-0ca0f0.netlify.app/" target="_blank" rel="noreferrer">Anastasiia </a> and is open-sourced on <a href="https://github.com/citronoiseau/react-weather-app" target="_blank" rel="noreferrer">GitHub </a> and
-      hosted on  <a href="https://unrivaled-croissant-9c2359.netlify.app/" target="_blank" rel="noreferrer"> Netlify </a> 
+      This project was coded by{" "}
+      <a
+        href="https://anastasiiatrubachova.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Anastasiia{" "}
+      </a>{" "}
+      and is open-sourced on{" "}
+      <a
+        href="https://github.com/citronoiseau/react-weather-app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        GitHub{" "}
+      </a>{" "}
+      and hosted on{" "}
+      <a
+        href="https://weatherapplicationjsreact.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {" "}
+        Netlify{" "}
+      </a>
     </div>
   );
 }
